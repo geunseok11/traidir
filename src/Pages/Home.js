0ae13693a1dde0d6c328c1537136f9a2da6227de
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-08-26 23:05:59 +0900

    UPDATE pages/home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
 const Home = () => {
+  useEffect(() => {}, []);
+
   return (
     <div>
-      
+      <link to="/beerlist"> BEER LIST</link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
