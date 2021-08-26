2017de190986619a72802c50cc1fa1dd01efcd79
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-08-26 23:06:14 +0900

    UPDATE routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,16 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BeerList from "./Pages/BeerList";
 import Home from "./Pages/Home";
 // import ReactGA from "react-ga";
 
 const Routes = () => {
-
   return (
     <Router>
-        <Switch>
-          <Route path="/home" component={Home} />
-        </Switch>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/beerlist" component={BeerList} />
+      </Switch>
     </Router>
   );
 };
