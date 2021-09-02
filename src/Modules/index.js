dc014b66148f66194497c2ee3ec753830a990c31
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-09-02 23:16:23 +0900

    FIX Modules/index

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import home from "./reducers/home";
 import homeSaga from "./home";
+import axios from "axios";
 
 //watcher saga -> actions -> worker saga
 // import loading from "./loading";
@@ -15,6 +16,9 @@ const rootReducer = combineReducers({
 
 // export default rootReducer;
 export default rootReducer;
+
+axios.defaults.baseURL = "https://api.punkapi.com/v2";
+axios.defaults.withCredentials = true;
 
 //wathcer saga
 export function* rootSaga() {
